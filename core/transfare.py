d481ef0a54ff572a8291847e4c459ddaef4623ac
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-08-20 20:56:32 +0300

    add authenticate Transfer using Pin Number

--- core/transfare.py
+++ core/transfare.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from account.models import Account
 from django.db.models import Q
+from .models import Transaction
 
 
 
@@ -40,3 +41,103 @@ def AmountTranfare(request, account_number):
 
 
     return render(request, 'transfare/amount-transfare.html', context)
+
+
+
+
+def AmountTranfareProcess(request, account_number):
+    account = Account.objects.get(account_number=account_number)
+    sender = request.user
+    reciver = account.user
+
+    sender_account = request.user.account
+    reciver_account = account
+
+
+    if request.method=="POST":
+        amount = request.POST.get("amount-send")
+        description = request.POST.get("description")
+
+        if sender_account.account_balance > 0 and amount:
+            new_transaction = Transaction.objects.create(
+                user = request.user,
+                amount = amount,
+                sender = sender,
+                reciver = reciver,
+                sender_account = sender_account,
+                reciver_account = reciver_account,
+                description = description,
+                transaction_type = "transfer",
+                status = "processing",
+            )
+
+            new_transaction.save()
+            transaction_id = new_transaction.transaction_id
+
+            return redirect("core:transfare-confirmation", account.account_number, transaction_id)
+        
+        else:
+            messages.warning(request, 'Insufficient fund')
+            return redirect("core:amount-transfare", account.account_number)
+
+    else:
+        messages.warning(request, 'Error Occured, Try again later .')
+        return redirect("account:account")
+
+         
+
+
+def TransactionConfirmation(request, account_number, transaction_id):
+    try:
+        account = Account.objects.get(account_number=account_number)
+        transaction = Transaction.objects.get(transaction_id=transaction_id)
+    except:
+        messages.warning(request, 'Transaction Does not exists')
+        return redirect('account:account') 
+
+
+    context = {'account':account,
+               'transaction':transaction
+               }       
+    
+    return render(request, 'transfare/transaction-confirmation.html', context)
+
+
+
+
+def TransfarProcess(request,account_number, transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+
+    sender = request.user
+    reciver = account.user
+
+    sender_account = request.user.account
+    reciver_account = account
+    
+    completed = False
+    if request.method=="POST":
+        pin_num = request.POST.get('pin-number')
+        print(pin_num)
+
+
+        if pin_num == sender_account.pin_number:
+
+            transaction.status = "completed"
+            transaction.save()
+
+            sender_account.account_balance -= transaction.amount
+            sender_account.save()
+            reciver_account.account_balance += transaction.amount
+            reciver_account.save()
+
+            messages.success(request, "Transfar Successfull.")
+            return redirect("account:account")
+        else:
+            messages.warning(request, "Incorrect Pin Number.")
+            return redirect("core:transfare-confirmation",account.account_number ,transaction.transaction_id)
+        
+    else:
+        messages.warning(request, "An Error occured, Try again later.")
+        return redirect("account:account")    
+
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index
-from .transfare import search_using_account, AmountTranfare
+from .transfare import search_using_account, AmountTranfare, AmountTranfareProcess,TransactionConfirmation,TransfarProcess
 
 
 app_name = 'core'
@@ -10,6 +10,10 @@ app_name = 'core'
 urlpatterns = [
     path("", index, name='index'),
     path('search-account/', search_using_account, name='search-account'),
-    path('amount-transfare/<account_number>/',AmountTranfare , name='amount-transfare')
+    path('amount-transfare/<account_number>/',AmountTranfare , name='amount-transfare'),
+    path('amount-transfare-process/<account_number>/',AmountTranfareProcess , name='amount-transfare-Process'),
+    path('transfare-confirm/<account_number>/<transaction_id>/',TransactionConfirmation , name='transfare-confirmation'),
+    path('transfare-process/<account_number>/<transaction_id>/',TransfarProcess , name='transaction-process'),
+
 
 ]

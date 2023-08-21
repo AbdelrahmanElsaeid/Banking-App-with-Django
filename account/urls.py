07d98616e800335e0cc60ecf3d8322d9e5e6f887
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-08-21 09:26:02 +0300

    add fun dashboard

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import kyc_registration, account
+from .views import kyc_registration, account,Dashboard
 
 
 
@@ -7,6 +7,8 @@ app_name = 'account'
 
 urlpatterns = [
     path("",account , name='account'),
+    path("dashboard",Dashboard , name='dashboard'),
+
     path("kyc-reg/",kyc_registration, name='kyc-reg'),
   
 
--- account/views.py
+++ account/views.py
@@ -58,3 +58,27 @@ def kyc_registration(request):
         }
 
     return render(request, "account/kyc-form.html", context)           
+
+
+
+
+
+def Dashboard(request):
+    user = request.user
+
+    if request.user.is_authenticated:
+        try:
+            kyc = KYC.objects.get(user=user)
+        except:
+            messages.warning(request, "You Need To Submit YOur KYC")
+            return redirect("account:kyc-form")
+
+        account = Account.objects.get(user=user)
+    else:
+        messages.warning(request, "You Need To Login")
+        return redirect("userauths:sign-in")
+    context = {
+        'account':account,
+        'kyc':kyc,
+    }
+    return render(request, 'account/dashboard.html', context)

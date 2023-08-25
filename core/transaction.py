57a44f0764c7c203cf84056a5f457acd65c23c61
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-08-25 12:06:25 +0300

    Settle users payment request using Django

--- core/transaction.py
+++ core/transaction.py
@@ -6,14 +6,20 @@ from .models import Transaction
 from django.contrib.auth.decorators import login_required
 
 
-
+@login_required
 def transaction_list(request):
-    transaction_sender = Transaction.objects.filter(sender = request.user).order_by("-id")
-    transaction_reciver = Transaction.objects.filter(reciver = request.user).order_by("-id")
+    transaction_sender = Transaction.objects.filter(sender = request.user, transaction_type="transfer").order_by("-id")
+    transaction_reciver = Transaction.objects.filter(reciver = request.user, transaction_type="transfer").order_by("-id")
+
+    request_sender_transaction = Transaction.objects.filter(sender=request.user, transaction_type="request")
+    request_reciever_transaction = Transaction.objects.filter(reciver=request.user, transaction_type="request")
 
     context = {
         "transaction_sender":transaction_sender,
         "transaction_reciver":transaction_reciver,
+        "request_sender_transaction":request_sender_transaction,
+        "request_reciever_transaction": request_reciever_transaction,
+
     }
 
 

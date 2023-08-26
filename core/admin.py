2dcf4e3827b8a0d8f339edc3cce984ce5e47249d
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2023-08-26 18:34:24 +0300

    add credit cards models

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Transaction
+from .models import Transaction, CreditCard
 # Register your models here.
 
 class TransactionAdmin(admin.ModelAdmin):
     list_editable = ['amount', 'status', 'transaction_type', 'reciver', 'sender']
     list_display = ['user', 'amount', 'status', 'transaction_type','reciver', 'sender']
-    
+
+class CreditCardAdmin(admin.ModelAdmin):
+    list_editable = ['amount', 'card_type']
+    list_display = ['user', 'amount', 'card_type']    
 
 admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(CreditCard, CreditCardAdmin)
--- core/models.py
+++ core/models.py
@@ -27,6 +27,14 @@ TRANSACTION_STATUS = (
 )
 
 
+
+CARD_TYPE = (
+    ("master", "master"),
+    ("visa", "visa"),
+    ("verve", "verve"),
+
+)
+
 class Transaction(models.Model):
     transaction_id = ShortUUIDField(unique = True, length = 15, max_length=20, prefix="TRN")
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user")
@@ -52,3 +60,25 @@ class Transaction(models.Model):
         except:
             return f"Transaction"
 
+
+
+
+class CreditCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_id = ShortUUIDField(unique=True, length=5, max_length=20, prefix="CARD", alphabet="1234567890")
+
+    name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    month = models.IntegerField()
+    year = models.IntegerField()
+    cvv = models.IntegerField()
+
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+
+    card_type = models.CharField(choices=CARD_TYPE, max_length=20, default="master")
+    card_status = models.BooleanField(default=True)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}"
